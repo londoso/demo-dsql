07219ae8e7ea74595f40db098fd661a574542920
Anderson Londoño <londoso@gmail.com>
2025-02-16 21:34:40 -0500

    get data

--- lambdas/get_data/app.py
+++ lambdas/get_data/app.py
@@ -2,15 +2,26 @@ from aws_lambda_powertools.event_handler import APIGatewayRestResolver
 from aws_lambda_powertools.utilities.typing import LambdaContext
 from aws_lambda_powertools.logging import correlation_paths
 from aws_lambda_powertools import Logger
+import boto3
+import os
 
 app = APIGatewayRestResolver()
 logger = Logger()
+
+region = os.environ["REGION"]
+cluster_endpoint = os.environ["DSQL_CLUSTER_ENDPOINT"]
+
+def generate_token(cluster_endpoint, region):
+    client = boto3.client("dsql", region_name=region)
+    token = client.generate_db_connect_admin_auth_token(cluster_endpoint, region)
+    print(token)
+    return token
 
 @app.get("/get-data")
 def get_data():
 
     logger.info("Hello world API - HTTP 200")
-    return {"message": "hello world"}
+    return {"message": generate_token(cluster_endpoint, region)}
 
 # Enrich logging with contextual information from Lambda
 @logger.inject_lambda_context(correlation_id_path=correlation_paths.API_GATEWAY_REST)
--- lambdas/insert_data/app.py
+++ lambdas/insert_data/app.py
@@ -2,33 +2,80 @@ from aws_lambda_powertools.event_handler import APIGatewayRestResolver
 from aws_lambda_powertools.utilities.typing import LambdaContext
 from aws_lambda_powertools.logging import correlation_paths
 from aws_lambda_powertools import Logger
-from aws_lambda_powertools import Tracer
-from aws_lambda_powertools import Metrics
-from aws_lambda_powertools.metrics import MetricUnit
+from aws_lambda_powertools.utilities.streaming.s3_object import S3Object
+import boto3
+import os
+import psycopg2
+
 
 app = APIGatewayRestResolver()
-tracer = Tracer()
 logger = Logger()
-metrics = Metrics(namespace="Powertools")
 
-@app.get("/hello")
-@tracer.capture_method
-def hello():
-    # adding custom metrics
-    # See: https://awslabs.github.io/aws-lambda-powertools-python/latest/core/metrics/
-    metrics.add_metric(name="HelloWorldInvocations", unit=MetricUnit.Count, value=1)
+region = os.environ["REGION"]
+cluster_endpoint = os.environ["DSQL_CLUSTER_ENDPOINT"]
+data_bucket = os.environ["DATA_BUCKET"]
 
-    # structured log
-    # See: https://awslabs.github.io/aws-lambda-powertools-python/latest/core/logger/
-    logger.info("Hello world API - HTTP 200")
-    return {"message": "hello world"}
+def generate_token(cluster_endpoint, region):
+    client = boto3.client("dsql", region_name=region)
+    token = client.generate_db_connect_admin_auth_token(cluster_endpoint, region)
+    return token
+
+def get_connection(cluster_endpoint):
+    conn = psycopg2.connect(dbname = "postgres",
+                        user = "admin", 
+                        host= cluster_endpoint,
+                        password = generate_token(cluster_endpoint, region),
+                        port = 5432,
+                        sslmode= 'require',
+                        connect_timeout=3)
+    return conn
+
+@app.get("/insert-data")
+def insert_data():
+
+    conn = get_connection(cluster_endpoint)
+    cursor = conn.cursor()
+
+    query = """
+            CREATE TABLE IF NOT EXISTS test_table( 
+                index int,
+                customer_id text,
+                first_name text,
+                last_name text,
+                company text,
+                city text,
+                country text,
+                phone1 text,
+                phone2 text,
+                email text,
+                subscription_date text,
+                website text);
+        """
+    cursor.execute(query)
+
+    query = """
+        INSERT INTO test_table VALUES (%(index)s, %(customer_id)s, %(first_name)s, %(last_name)s, 
+            %(company)s, %(city)s, %(country)s, %(phone1)s, %(phone2)s, %(email)s, 
+            %(subscription_date)s, %(website)s)
+        ;
+        """
+
+    s3 = S3Object(bucket=data_bucket, key="customers-10.csv", is_csv=True)
+    
+    for line in s3:
+        logger.info(line)
+        cursor.execute(query, line)
+
+    # Make the changes to the database persistent
+    conn.commit()
+    # Close cursor and communication with the database
+    cursor.close()
+    # Close connection
+    conn.close()
+    return {"message": "Registros insertados en la base de datos"}
 
 # Enrich logging with contextual information from Lambda
 @logger.inject_lambda_context(correlation_id_path=correlation_paths.API_GATEWAY_REST)
-# Adding tracer
-# See: https://awslabs.github.io/aws-lambda-powertools-python/latest/core/tracer/
-@tracer.capture_lambda_handler
-# ensures metrics are flushed upon request completion/failure and capturing ColdStart metric
-@metrics.log_metrics(capture_cold_start_metric=True)
+
 def lambda_handler(event: dict, context: LambdaContext) -> dict:
     return app.resolve(event, context)
